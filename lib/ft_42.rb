39b0e4ec4ecb3e81c4371fa5fec4feb6bafb241d
Matias Fernandez <matiasfmolinari@gmail.com>
2016-11-06 23:12:23 -0800

    commence architecture design

--- lib/ft_42.rb
+++ lib/ft_42.rb
@@ -4,17 +4,30 @@ require "pastel"
 require "oauth2"
 require "ruby-progressbar"
 
+
+module Constants
+  URL_42       = "https://api.intra.42.fr"
+  UID_42       = ENV.fetch("FT42_UID")
+  SECRET_42    = ENV.fetch("FT42_SECRET")
+  HOURS_NEEDED = 38
+end
+
 class FT_42
+
+  include Constants
+
   def initialize(*args)
-    url_42       = "https://api.intra.42.fr"
-    uid_42       = ENV.fetch("FT42_UID")
-    secret_42    = ENV.fetch("FT42_SECRET")
-    hours_needed = 38
+    # Setup API
+    # Make requests
+    # Parse Requests
+    # Print
+
+
 
     username = args.first
 
     # get token
-    client = OAuth2::Client.new(uid_42, secret_42, site: url_42)
+    client = OAuth2::Client.new(UID_42, SECRET_42, site: URL_42)
     token  = client.client_credentials.get_token
 
     # helpers
@@ -64,7 +77,7 @@ class FT_42
 
     puts "Has " + pastel.bright_green.bold("#{hours} #{hours == 1 ? 'hour' : 'hours'}") + " in the clusters this week, starting #{Time.current.beginning_of_week.strftime("%A, %B #{Time.current.beginning_of_week.day.ordinalize}")}. #{'Go to sleep.' if hours > 60}"
 
-    percent_complete = ((hours.to_f / hours_needed.to_f) * 100).round
+    percent_complete = ((hours.to_f / HOURS_NEEDED.to_f) * 100).round
     if (percent_complete <= 100)
       progressbar_needed = ProgressBar.create(progress_mark: "█", length: 60, format: "%t: |" + pastel.red("%B") + "| #{hours}/38 hours")
       percent_complete.times { progressbar_needed.increment }
@@ -90,16 +103,100 @@ class FT_42
   end
 end
 
-class API_42
+class Client
+  attr_reader :username
+
+  def initialize(username)
+    @username = username
+  end
+
+  def user
+  end
+
+  def user_sessions_this_week
+  end
+end
+
+class Token
+  def initialize(uid, secret, url)
+    
+  end
 end
 
 class User
+
+  attr_reader :user
+
+  def initialize(user_response)
+    @user = user_response
+  end
+
+  def current_projects
+  end
+
+  def first_name
+  end
+
+  def last_name
+  end
+
+  def full_name
+  end
+
+  def correction_points
+  end
+
+  def level
+  end
+
+  def phone
+  end
+
+  def active?
+  end
+
+  def last_active_session
+  end
+
+  def hours_this_week
+  end
 end
 
 class Session
+  attr_reader :session
+
+  def initialize(session)
+    @session = session
+  end
+
+  def host
+  end
+
+  def duration_in_hours
+  end
+
+
+end
+
+class UserSessions
+  attr_reader :user_sessions
+
+  def initialize(user_sessions_response)
+    @user_sessions = user_sessions_response
+  end
+
+  def total_hours_this_week
+  end
 end
 
 class UserPrinter
+  attr_reader :pastel
+
+  # include ActiveView and ActiveSupport
+
+  def initialize
+    @pastel = Pastel.new
+  end
 end
 
 class SessionPrinter
