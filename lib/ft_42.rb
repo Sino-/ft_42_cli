bec6bc2b3162bfcf41bfe44b5dbb184fbc5335ce
Matias Fernandez <matiasfmolinari@gmail.com>
2016-12-01 15:51:57 -0800

    add user images

--- lib/ft_42.rb
+++ lib/ft_42.rb
@@ -39,7 +39,9 @@ class FT_42
       user_print          = UserPrinter.new(user)
       user_sessions_print = UserSessionsPrinter.new(user_sessions)
       if args.size == 1
-        system "iterm2-viewer /nfs/intrav2cdn/users/medium_#{args.first}.jpeg"
+        if ENV["TERM_PROGRAM"] == "iTerm.app"
+          system "iterm2-viewer /nfs/intrav2cdn/users/medium_#{args.first}.jpeg"
+        end
         user_print.all
         user_sessions_print.all
       elsif args.second == "sessions"
